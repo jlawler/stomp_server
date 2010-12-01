cd03852b31e8eedc5eac545044ca5101e834e4fd
tyler <tyler.green2@gmail.com>
2010-12-01 16:24:11 -0600

    added command line options for timeouts, removed more debug prints

--- lib/stomp_server/queue/popable.rb
+++ lib/stomp_server/queue/popable.rb
@@ -1,15 +1,10 @@
 module Popable
   def popwhile dest
     frame = self.dequeue dest
-    puts "popping #{ frame }"
     while frame && yield(frame) 
       frame = self.dequeue dest
     end
-    
-    if frame
-      self.requeue(dest, frame) 
-      puts "requeuing #{ frame }"
-    end
+    self.requeue(dest, frame) if frame
     self
   end
 end
